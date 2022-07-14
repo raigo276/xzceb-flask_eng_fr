8aa0a2a47f0a29c1d93573bef753efde8c77b41e
raigo276 <rainel.gonzalez@web.de>
2022-07-14 15:37:02 +0000

    Final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -3,18 +3,27 @@ import unittest
 from translator import english_to_french, french_to_english
 
 class TestEnglishToFrench(unittest.TestCase):
-    def test_english_to_french(self):
+    def test_english_to_french_isequal(self):
         self.assertEqual(english_to_french("Hello"), "Bonjour")
-        self.assertEqual(english_to_french("Coding"), "Codage")
+    def test_english_to_french_isnotequal(self):
+        self.assertNotEqual(english_to_french("Hello"), "Hola")
+    def test_english_to_french_isnone(self):
         self.assertIsNone(english_to_french(None))
+    def test_english_to_french_isnone_wheninputempty(self):
         self.assertIsNone(english_to_french(''))
 
 class TestFrenchToEnglish(unittest.TestCase):
-    def test_french_to_english(self):
+    def test_french_to_english_isequal(self):
         self.assertEqual(french_to_english("Bonjour"), "Hello")
-        self.assertEqual(french_to_english("Codage"), "Encoding")
+    def test_french_to_english_isnotequal(self):
+        self.assertNotEqual(french_to_english("Bonjour"), "Hola")
+    def test_french_to_english_isnone(self):
         self.assertIsNone(french_to_english(None))
+    def test_french_to_english_isnone_wheninputempty(self):
         self.assertIsNone(french_to_english(''))
 
 
-unittest.main()
+suite = unittest.TestLoader().loadTestsFromTestCase(TestEnglishToFrench)
+unittest.TextTestRunner(verbosity=2).run(suite)
+suite = unittest.TestLoader().loadTestsFromTestCase(TestFrenchToEnglish)
+unittest.TextTestRunner(verbosity=2).run(suite)
